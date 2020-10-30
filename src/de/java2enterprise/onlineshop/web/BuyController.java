621426f4de98162bff14ec7919eccac2f4d2b89f
Irina <irina.schubert@students.ffhs.ch>
2020-10-30 15:12:43 +0100

    sold item gets status "sold" and disappears from search view

--- src/de/java2enterprise/onlineshop/web/BuyController.java
+++ src/de/java2enterprise/onlineshop/web/BuyController.java
@@ -5,17 +5,21 @@ import java.time.LocalDateTime;
 import java.util.logging.Logger;
 
 import javax.annotation.Resource;
+import javax.ejb.EJB;
 import javax.el.ELContext;
 import javax.el.ELResolver;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 
+import de.java2enterprise.onlineshop.ejb.SellBeanLocal;
 import de.java2enterprise.onlineshop.model.Customer;
 import de.java2enterprise.onlineshop.model.Item;
+import de.java2enterprise.onlineshop.model.Status;
 
 @Named
 @RequestScoped
@@ -30,6 +34,12 @@ public class BuyController implements Serializable {
 
     @Resource
     private UserTransaction ut;
+    
+    @Inject
+    private Status status;
+    
+    @EJB
+    private SellBeanLocal sellBeanLocal;
 
     public String buyItem(Long id) {
         FacesContext ctx = FacesContext.getCurrentInstance();
@@ -44,9 +54,11 @@ public class BuyController implements Serializable {
         Customer customer = signinController.getCustomer();
         try {
             ut.begin();
+            status = sellBeanLocal.findStatus(3L);
             Item item = em.find(Item.class, id);
             item.setBuyer(customer);
             item.setSold(LocalDateTime.now());
+            item.setStatus(status);
             ut.commit();
             log.info(item + " bought by " + customer.getEmail());
         } catch (Exception e) {
