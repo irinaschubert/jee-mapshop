97ce16d2b539fef949a1f7b3d637a1a72e1c751b
Irina <irina.schubert@students.ffhs.ch>
2020-11-09 19:16:15 +0100

    deregister fix

--- db/fill_db.sql
+++ db/fill_db.sql
@@ -2,3 +2,5 @@ insert into mapshop.status (id,description) values (mapshop.seq_status.NEXTVAL,'
 insert into mapshop.status (id,description) values (mapshop.seq_status.NEXTVAL,'inactive');
 insert into mapshop.status (id,description) values (mapshop.seq_status.NEXTVAL,'sold');
 insert into mapshop.status (id,description) values (mapshop.seq_status.NEXTVAL,'reserved');
+insert into mapshop.customer (id,email,password) values (mapshop.seq_customer.NEXTVAL,'a@mail.ch','asdfasdf');
+insert into mapshop.customer (id,email,password) values (mapshop.seq_customer.NEXTVAL,'b@mail.ch','asdfasdf');
